5ac981b2ef4a2c3b8719c43209a6e5a7a74ec5a8
xieweiming <3052467868@qq.com>
2017-09-01 11:50:26 +0800

    发布故事板

---
+++ hotelApp/HotelIssueViewController.h
@@ -0,0 +1,13 @@
+//
+//  HotelIssueViewController.h
+//  hotelApp
+//
+//  Created by mac on 2017/9/1.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HotelIssueViewController : UIViewController
+
+@end
