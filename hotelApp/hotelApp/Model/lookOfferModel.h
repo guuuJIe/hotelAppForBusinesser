70cdfdc4589eb1f6448277fd2d5b17b1145f7815
wangzhangcan <1981701342@qq.com>
2017-09-07 09:15:03 +0800

    网络请求

---
+++ hotelApp/hotelApp/Model/lookOfferModel.h
@@ -0,0 +1,13 @@
+//
+//  lookOfferModel.h
+//  hotelApp
+//
+//  Created by admin2017 on 2017/9/6.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface lookOfferModel : NSObject
+
+@end
---
+++ hotelApp/hotelApp/Model/lookOfferModel.m
@@ -0,0 +1,13 @@
+//
+//  lookOfferModel.m
+//  hotelApp
+//
+//  Created by admin2017 on 2017/9/6.
+//  Copyright © 2017年 Education. All rights reserved.
+//
+
+#import "lookOfferModel.h"
+
+@implementation lookOfferModel
+
+@end
