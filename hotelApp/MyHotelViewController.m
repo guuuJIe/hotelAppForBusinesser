33efb42152933a2de0869aa38ca9e7abb0490492
xieweiming <3052467868@qq.com>
2017-09-08 20:14:34 +0800

    酒店发布接口

--- hotelApp/MyHotelViewController.m
+++ hotelApp/MyHotelViewController.m
@@ -10,6 +10,7 @@
 #import "MyHotelTableViewCell.h"
 #import "HotelIssueViewController.h"
 #import "HotelModel.h"
+#import <UIImageView+WebCache.h>
 
 @interface MyHotelViewController ()<UITableViewDataSource,UITableViewDelegate>
 
@@ -21,7 +22,8 @@
 @property (strong, nonatomic) UIActivityIndicatorView *avi;//蒙层
 @property (strong, nonatomic) NSMutableArray *tableArr;
 @property (strong, nonatomic) NSMutableArray *roomInfoArr;
-//@property (strong, nonatomic) HotelModel *hotelModel;
+@property (strong, nonatomic) NSArray *arr;
+@property (strong, nonatomic) HotelModel *hotelModel;
 
 @end
 
@@ -90,6 +92,7 @@
     [self myHotelRequest];
 }
 
+//查看自己发布的酒店接口
 - (void)myHotelRequest {
     //参数
     NSDictionary *para = @{@"business_id" : @1};
@@ -103,14 +106,19 @@
         [_avi stopAnimating];
         if([responseObject[@"result"] integerValue] == 1) {
             NSArray *content = responseObject[@"content"];
+            //每次请求成功先把数组清空再插入数据
+            [_tableArr removeAllObjects];
             //遍历content
             for (NSDictionary *dict in content) {
                 NSString *roomInfoJSONStr = dict[@"hotel_type"];
                 id hotelType = [roomInfoJSONStr JSONCol];
+                //NSLog(@"hotelType%@",hotelType);
                 [_roomInfoArr addObject:hotelType];
+                
                 HotelModel *hotelModel = [[HotelModel alloc] initWithDict:dict];
                 [_tableArr addObject:hotelModel];
             }
+           
             //重载数据
             [_myHotelTableView reloadData];
         } else {
@@ -121,13 +129,45 @@
         }
         
     } failure:^(NSInteger statusCode, NSError *error) {
-        //当网络请求成功的时候停止动画(菊花膜/蒙层停止转动消失)
+        //当网络请求失败的时候停止动画(菊花膜/蒙层停止转动消失)
         [_avi stopAnimating];
         UIRefreshControl *ref = (UIRefreshControl *)[_myHotelTableView viewWithTag:10005];
         [ref endRefreshing];
         //失败提示框
         [Utilities popUpAlertViewWithMsg:@"网络错误，请稍后再试" andTitle:@"提示" onView:self];
     }];
+}
+
+//删除酒店接口
+- (void)deleteHotelRequest {
+    //参数
+    NSDictionary *para = @{@"id" : @1};
+    //网络请求
+    [RequestAPI requestURL:@"/deleteHotel" withParameters:para andHeader:nil byMethod:kGet andSerializer:kForm success:^(id responseObject) {
+        UIRefreshControl *ref = (UIRefreshControl *)[_myHotelTableView viewWithTag:10005];
+        [ref endRefreshing];
+        //成功以后要做的事情在此处执行
+        NSLog(@"删除酒店：%@", responseObject);
+        //当网络请求成功的时候停止动画(菊花膜/蒙层停止转动消失)
+        [_avi stopAnimating];
+        if([responseObject[@"result"] integerValue] == 1) {
+            
+        } else {
+            [_avi stopAnimating];
+            //业务逻辑失败的情况下
+            NSString *errorMsg = [ErrorHandler getProperErrorString:[responseObject[@"result"] integerValue]];
+            [Utilities popUpAlertViewWithMsg:errorMsg andTitle:nil onView:self];
+        }
+        
+    } failure:^(NSInteger statusCode, NSError *error) {
+        //当网络请求失败的时候停止动画(菊花膜/蒙层停止转动消失)
+        [_avi stopAnimating];
+        UIRefreshControl *ref = (UIRefreshControl *)[_myHotelTableView viewWithTag:10005];
+        [ref endRefreshing];
+        //失败提示框
+        [Utilities popUpAlertViewWithMsg:@"网络错误，请稍后再试" andTitle:@"提示" onView:self];
+    }];
+
 }
 
 
@@ -144,10 +184,38 @@
 
 //设置每一组中每一行的细胞长什么样
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    //图片显示
+    NSString *userAgent = @"";
+    userAgent = [NSString stringWithFormat:@"%@/%@ (%@; iOS %@; Scale/%0.2f)", [[[NSBundle mainBundle] infoDictionary] objectForKey:(__bridge NSString *)kCFBundleExecutableKey] ?: [[[NSBundle mainBundle] infoDictionary] objectForKey:(__bridge NSString *)kCFBundleIdentifierKey], [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"] ?: [[[NSBundle mainBundle] infoDictionary] objectForKey:(__bridge NSString *)kCFBundleVersionKey], [[UIDevice currentDevice] model], [[UIDevice currentDevice] systemVersion], [[UIScreen mainScreen] scale]];
+    
+    if (userAgent) {
+        if (![userAgent canBeConvertedToEncoding:NSASCIIStringEncoding]) {
+            NSMutableString *mutableUserAgent = [userAgent mutableCopy];
+            if (CFStringTransform((__bridge CFMutableStringRef)(mutableUserAgent), NULL, (__bridge CFStringRef)@"Any-Latin; Latin-ASCII; [:^ASCII:] Remove", false)) {
+                userAgent = mutableUserAgent;
+            }
+        }
+        [[SDWebImageDownloader sharedDownloader] setValue:userAgent forHTTPHeaderField:@"User-Agent"];
+    }
+    
     MyHotelTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"myHotelCell" forIndexPath:indexPath];
     HotelModel *hotelModel = _tableArr[indexPath.row];
     cell.hotelNameLabel.text = hotelModel.hotelName;
-    
+    cell.priceLabel.text = [NSString stringWithFormat:@"价格¥:%ld",(long)hotelModel.price];
+    NSURL *url = [NSURL URLWithString:hotelModel.hotelImgs];
+    [cell.hotelImgView sd_setImageWithURL:url placeholderImage:[UIImage imageNamed:@"hotel_img"]];
+  
+    if (_arr.count == 4) {
+        //遍历数组
+        for (_arr in _roomInfoArr) {
+            cell.describeLabel.text = [NSString stringWithFormat:@"描述:%@",_arr[1]];
+            cell.bedTypeLabel.text = _arr[2];
+            //截取字符串
+            NSString *str = [_arr[3] substringToIndex:2];
+            cell.areaLabel.text = [NSString stringWithFormat:@"面积:%@平米",str];
+        }
+
+    }
     return cell;
 }
 
@@ -161,6 +229,47 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     //取消选中
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
+//设置细胞被编辑时执行什么操作
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    //判断当前的编辑是否属于删除行为
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        //判断当前的tableView是否myHotelTableView
+        if (tableView == _myHotelTableView) {
+            //创建弹窗
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示" message:@"你确定删除该条发布？" preferredStyle:UIAlertControllerStyleAlert];
+            //创建取消按钮
+            UIAlertAction *actionA = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+                //刷新表格，重载数据
+                [_myHotelTableView reloadData];
+            }];
+            //创建确定按钮
+            UIAlertAction *actionB = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action){
+                //调用删除酒店网络接口
+                [self deleteHotelRequest];
+                [_tableArr removeObjectAtIndex:indexPath.row];
+                [_myHotelTableView beginUpdates];
+                //删除界面上的细胞
+                [_myHotelTableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+                [_myHotelTableView endUpdates];
+                //刷新表格，重载数据
+                //[_delectTableView reloadData];
+                [self myHotelRequest];
+            }];
+            [alert addAction:actionA];
+            [alert addAction:actionB];
+            //在当前页面显示弹框
+            [self presentViewController:alert animated:YES completion:nil];
+        }
+        
+    }
+}
+
+//把英文删除变成中文删除
+- (nullable NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return @"删除";
 }
 
 
--- hotelApp/hotelApp/Model/HotelModel.m
+++ hotelApp/hotelApp/Model/HotelModel.m
@@ -14,10 +14,13 @@
 - (instancetype)initWithDict: (NSDictionary *)dict {
     self = [super init];
     if (self) {
-        _businessId = [[Utilities nullAndNilCheck:dict[@"business_id"] replaceBy:0] integerValue];
+        _Id = [[Utilities nullAndNilCheck:dict[@"id"] replaceBy:0] integerValue];
+        _businessId = [Utilities nullAndNilCheck:dict[@"business_id"] replaceBy:@""] ;
         _hotelName = [Utilities nullAndNilCheck:dict[@"hotel_name"] replaceBy:@""];
         _hotelType = [Utilities nullAndNilCheck:dict[@"hotel_type"] replaceBy:@""];
-        _hotelImgs = [Utilities nullAndNilCheck:dict[@"room_imgs"] replaceBy:@""];
+        _hotelImgs = [Utilities nullAndNilCheck:dict[@"hotel_img"] replaceBy:@""];
+        _price = [[Utilities nullAndNilCheck:dict[@"price"] replaceBy:0] integerValue];
+        
     }
     return self;
 }
